e171af90a6a8aa1dac8c5caf645de1d12b0ccdb4
Amanuel <amanuelmoha.official@gmail.com>
2024-05-02 10:59:59 +0300

    connect to mongoose

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+  }
 const express = require("express")
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
@@ -15,6 +18,15 @@ app.use(expressLayouts)
 
 //public files locations
 app.use(express.static('public'))
+
+//set up mongoose
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL,{
+    useNewUrlParser:true
+})
+const db = mongoose.connection
+db.on('error',error => console.error(error))
+db.once('open',() => console.log('connected to mongoose!'))
 
 
 // fetch from main.js
--- routes/main.js
+++ routes/main.js
@@ -3,11 +3,12 @@ const router = express.Router()
 
 
 router.get("/", (req, res) => {
-    res.send("Hello World!!")
+    //res.send("Hello World!!")
+    res.render('main')
   })
 
 
-  
+
   
   module.exports = router
   
