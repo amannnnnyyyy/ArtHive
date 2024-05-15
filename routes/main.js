e29b26ede13914b2337851777fc0a1b6abb543f0
Amanuel <amanuelmoha.official@gmail.com>
2024-05-15 16:16:23 +0300

    recently added Page

--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,18 @@
 const express = require("express")
 const router = express.Router()
+const Og_writtenWorks = require("../models/writtenWork")
 
 
-router.get("/", (req, res) => {
-    //res.send("Hello World!!")
-    res.render('main')
+router.get("/", async(req, res) => {
+  let writtenWorks
+  try{
+    writtenWorks = await Og_writtenWorks.find().sort({createdAt:'desc'}).limit(10).exec()
+    console.log(writtenWorks)
+  }catch(err){
+    console.log(err)
+    writtenWorks = []
+  }
+  res.render('main.ejs',{writtenWorks:writtenWorks})
   })
 
 
